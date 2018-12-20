0045b02aefebb06fdb02088036517949aadf8c44
jamesaldridge <jamesaldridge711@gmail.com>
2018-12-20 02:12:22 +0000

    Improved Homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,41 +1,60 @@
 <template>
-  <div class="hero">
-    <h1 class="vue-title">AceAnalysis</h1>
+  <div>
+    <b-carousel id="carousel1"
+                style="text-shadow: 1px 1px 2px #333;"
+                controls
+                indicators
+                background="#ababab"
+                :interval="4000"
+                img-width="1024"
+                img-height="480"
+                v-model="slide"
+                @sliding-start="onSlideStart"
+                @sliding-end="onSlideEnd"
+    >
 
-    <table align="center">
-      <tr>
-        <td>
-          <p class="lead"><b><i>Poker Analytics</i></b>
-          <p>information will go here...</p>
-          <p>Check out player Hud stats <a href ="https://www.pokernews.com/strategy/a-guide-to-setting-up-your-online-poker-heads-up-display-hud-19892.htm">here</a></p>
-          <p>Keep track of tournament earnings!</p>
-        </td>
-        <td>
-          <img src="../assets/fourOfAKindAces.gif" alt="description here" />
-        </td>
-      </tr>
-    </table>
+      <b-carousel-slide caption="AceAnalysis Aims to elevate your game"
+                        text="Improve Your game and move up the stakes"
+                        img-src="http://www.christian--louboutin.in.net/wp-content/uploads/2018/08/poker-chips-on-the-table-1024x480.jpeg"
+      ></b-carousel-slide>
+
+      <b-carousel-slide
+        caption="Understand The Stats"
+        text="Overcome variance and make profit"
+        img-src="http://farm8.staticflickr.com/7417/11924718544_5407e07f89_b.jpg">
+      </b-carousel-slide>
+
+      <b-carousel-slide
+        caption="Crush the Game Online"
+        text="Build a bankroll"
+        img-src="https://bitcoinplay.net/wp-content/uploads/2018/10/slika-1-1024x480.jpg">
+      </b-carousel-slide>
+
+      <b-carousel-slide
+        caption="Become A Pro!"
+        text="Lose Small, Win Big"
+        img-src="https://d1nz104zbf64va.cloudfront.net/pv/i/o/579744c0d3904342238b4577."></b-carousel-slide>
+
+    </b-carousel>
   </div>
 </template>
 
-<style>
-  .hero {
-    height: 100vh;
-    margin-top: 30px;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
+<script>
+export default {
+  data () {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart (slide) {
+      this.sliding = true
+    },
+    onSlideEnd (slide) {
+      this.sliding = false
+    }
   }
-  .hero .lead {
-    font-weight: 200;
-    font-size: 2.5rem;
-  }
-  #app1 {
-    width: 60%;
-    margin: 0 auto;
-  }
-  .vue-title {
-    font-size: 70pt;
-    margin-bottom: 10px;
-  }
-</style>
+}
+</script>
+<!-- carousel-1.vue -->
